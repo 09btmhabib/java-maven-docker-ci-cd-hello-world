d61915b78c82ec3876eded3530bf5d1ede17cb8f
09btmhabib <mazhar.habib250@gmail.com>
2023-09-16 13:19:10 +0500

    Change image and optimized the pipeline

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM openjdk:17.0.1-jdk-slim
 
-EXPOSE 8080
-
 WORKDIR /applications
 
 COPY target/spring-github-actions-0.0.1.jar /applications/application.jar
--- src/main/java/com/mhabib/javamavendockercicdhelloworld/SpringGithubActionsApp.java
+++ src/main/java/com/mhabib/javamavendockercicdhelloworld/SpringGithubActionsApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaMavenDockerCiCdHelloWorldApplication {
+public class SpringGithubActionsApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaMavenDockerCiCdHelloWorldApplication.class, args);
+		SpringApplication.run(SpringGithubActionsApp.class, args);
 	}
 
 }
