269668e5138a7f27e99095dcc8fc636c7c04b6e1
09btmhabib <mazhar.habib250@gmail.com>
2023-09-16 23:44:43 +0500

    Adding helm chart

--- src/main/java/com/mhabib/springgithubactions/HelloWorldController.java
+++ src/main/java/com/mhabib/springgithubactions/HelloWorldController.java
@@ -1,13 +1,16 @@
 package com.mhabib.springgithubactions;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+ @Value("${name:default}")
+ private String var1;
 
  @GetMapping("/hello")
  public String sayHello(){
-  return "Hello I am alive here";
+  return "Hello I am alive here "+var1;
  }
 }
